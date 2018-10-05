4c9ba7db29c1b51d49c8b61364532b5037d9e202
BATHTUBGERALT1 <43067557+BATHTUBGERALT1@users.noreply.github.com>
2018-10-05 14:52:58 +0100

    camera follows player

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //public variables visible in unity
+    public TextMesh displayText;
+
+    //private variables cant be touched by other scripts
+    private int currentValue = 0;
+
+    //UPDATE BOTH THE DATA VALUE AND THE VISUAL DISPLAY 
+    public void ChangeValue(int _toChange)
+    {
+
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+
+    }
+
+    //reset the score to zero 
+    public void ResetScore()
+    {
+        currentValue = 0;
+        displayText.text = currentValue.ToString();
+    }
+}
